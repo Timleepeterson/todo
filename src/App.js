51a28af0fa92493a6d5f6e8268f7288b08330652
Tim Peterson <timleepeterson@gmail.com>
2022-03-27 20:05:58 -0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [toDo, setTodo] = useState("");
+  const [toDoArr, setToDoArr] = useState([])
+  const addTodo = (e) => {
+    e.preventDefault();
+    const newToDo = {
+      toDoItem: toDo,
+      complete: false
+    };
+    setToDoArr([...toDoArr, newToDo]);
+    setTodo("");
+  };
+
+  
+
+  const deleteToDo = (indexFromBelow) => {
+    const filteredToDo = toDoArr.filter((toDo, idx) => {
+      return indexFromBelow !== idx;
+    });
+    setToDoArr(filteredToDo)
+  };
+
+  const updateComplete = (idx) => {
+    const toDo = toDo.map((toDo, index) => {
+      if (idx === index){
+        toDo.complete = !toDo.complete;
+      }
+      return toDo;
+    });
+  }
+
+  
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addTodo}>
+        <label>
+          To Do
+          <input
+          value={toDo}
+          type="text"
+          onChange={(e) => setTodo(e.target.value)} />{" "}
+        </label>
+        <button type="submit">SUBMIT</button>
+      </form>
+      <h2>To Do List</h2>
+      {toDoArr.map((toDo, index) => {
+        return (
+        <div key={index}>
+          <h2>{toDo.toDoItem}</h2>
+
+          <input type="checkbox" onChange={() => updateComplete(toDo)}></input>
+            
+            
+            
+            <button
+            onClick={() => deleteToDo(index)}
+            style={{color: 'pink', backgroundColor: 'black'}}>DELETE
+            </button>
+          <hr />
+        </div>
+        );
+      })}
     </div>
   );
 }
